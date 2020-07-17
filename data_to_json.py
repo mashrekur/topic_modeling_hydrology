d616bce56e8aec70bb49689dd35966dabb2381f6
mashrekur <mrahman18@crimson.ua.edu>
2020-07-17 03:38:37 -0500

    modified data to json

--- data_to_json.py
+++ data_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import numpy as np
@@ -11,13 +11,13 @@ from gensim.models import LdaModel
 import json
 
 
-# In[ ]:
+# In[2]:
 
 
 nTopics = 25
 
 
-# In[ ]:
+# In[3]:
 
 
 # Load model
@@ -34,88 +34,110 @@ with open('data/raw_corpus.pkl', 'rb') as f:
     corpus_df = pkl.load(f)
 
 
-# In[ ]:
+# In[4]:
 
 
-topic_distributions.shape
+#convert all nans to zeros and all zeros to a very small number
+# topic_distributions = np.nan_to_num(topic_distributions)
+topic_distributions = np.where(topic_distributions == 0, 0.000001, topic_distributions)
+# topic_distributions
 
 
-# In[ ]:
+# In[5]:
 
 
-# Define topic names
-topic_names = [
-    'Precip Variability & Extr',
-    'Hydrogeochemistry',
-    'Uncertainty',
-    'Soil Moisture',
-    'Statistical Hydrology',
-    'Rainfall-Runoff',
-    'Precip Observation',
-    'Modeling & Calibration',
-    'Water Management',
-    'Snow Hydrology',
-    'Streamflow Processes',
-    'Water Quality',
-    'Channel Flow',
-    'Floods',
-    'Sediment & Erosion',
-    'Climate Change',
-    'Subsurface Flow & Trans',
-    'Scaling & Spatial Variabil',
-    'Land Surface Fluxes',
-    'Hydrogeology',
-    'Human Interv & Eff',
-    'Land Cover',
-    'Systems Hydrology',
-    'Modeling & Forecasting',
-    'Groundwater'
-]
+# np.where(topic_distributions == 0)[0]
 
 
-# In[ ]:
+# In[6]:
 
 
-# Define colors to associate with each topic
-custom_colors = {
- 'burlywood': '#DEB887',
- 'chocolate': '#D2691E',
- 'crimson': '#DC143C',
- 'darkgreen': '#006400',
- 'darkorange': '#FF8C00',
- 'darkslategrey': '#2F4F4F',
- 'deepskyblue': '#00BFFF',
- 'dimgray': '#696969',
- 'firebrick': '#B22222',
- 'gold': '#FFD700',
- 'goldenrod': '#DAA520',
- 'lawngreen': '#7CFC00',
- 'lightcoral': '#F08080',
- 'lightpink': '#FFB6C1',
- 'mediumvioletred': '#C71585',
- 'orangered': '#FF4500',
- 'orchid': '#DA70D6',
- 'royalblue': '#4169E1',
- 'slateblue': '#6A5ACD',
- 'springgreen': '#00FF7F',
- 'steelblue': '#4682B4',
- 'teal': '#008080',
- 'turquoise': '#40E0D0',
- 'yellow': '#FFFF00',
- 'blueviolet': '#8A2BE2',
- 'yellowgreen': '#9ACD32'}
-
-# turn into a list
-colorlist = []
-for i, color in enumerate(custom_colors.values()):
-    colorlist.append(tuple(int(color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)))
-    colorlist[i] = (colorlist[i][0] / 256, colorlist[i][1] / 256, colorlist[i][2] / 256)
+# smaller_set = topic_distributions[:100,:]
 
 
-# In[ ]:
+# In[7]:
+
+
+# corpus_df['Title'][:100]
+
+
+# In[8]:
+
+
+# # Define topic names
+# topic_names = [
+#     'Precip Variability & Extr',
+#     'Hydrogeochemistry',
+#     'Uncertainty',
+#     'Soil Moisture',
+#     'Statistical Hydrology',
+#     'Rainfall-Runoff',
+#     'Precip Observation',
+#     'Modeling & Calibration',
+#     'Water Management',
+#     'Snow Hydrology',
+#     'Streamflow Processes',
+#     'Water Quality',
+#     'Channel Flow',
+#     'Floods',
+#     'Sediment & Erosion',
+#     'Climate Change',
+#     'Subsurface Flow & Trans',
+#     'Scaling & Spatial Variabil',
+#     'Land Surface Fluxes',
+#     'Hydrogeology',
+#     'Human Interv & Eff',
+#     'Land Cover',
+#     'Systems Hydrology',
+#     'Modeling & Forecasting',
+#     'Groundwater'
+# ]
+
+
+# In[9]:
+
+
+# # Define colors to associate with each topic
+# custom_colors = {
+#  'burlywood': '#DEB887',
+#  'chocolate': '#D2691E',
+#  'crimson': '#DC143C',
+#  'darkgreen': '#006400',
+#  'darkorange': '#FF8C00',
+#  'darkslategrey': '#2F4F4F',
+#  'deepskyblue': '#00BFFF',
+#  'dimgray': '#696969',
+#  'firebrick': '#B22222',
+#  'gold': '#FFD700',
+#  'goldenrod': '#DAA520',
+#  'lawngreen': '#7CFC00',
+#  'lightcoral': '#F08080',
+#  'lightpink': '#FFB6C1',
+#  'mediumvioletred': '#C71585',
+#  'orangered': '#FF4500',
+#  'orchid': '#DA70D6',
+#  'royalblue': '#4169E1',
+#  'slateblue': '#6A5ACD',
+#  'springgreen': '#00FF7F',
+#  'steelblue': '#4682B4',
+#  'teal': '#008080',
+#  'turquoise': '#40E0D0',
+#  'yellow': '#FFFF00',
+#  'blueviolet': '#8A2BE2',
+#  'yellowgreen': '#9ACD32'}
+
+# # turn into a list
+# colorlist = []
+# for i, color in enumerate(custom_colors.values()):
+#     colorlist.append(tuple(int(color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)))
+#     colorlist[i] = (colorlist[i][0] / 256, colorlist[i][1] / 256, colorlist[i][2] / 256)
+
+
+# In[10]:
 
 
 #calculate JSD for all pairs of papers
+#the max force values (dist) are capped to 1000 later on
 def calc_KL_divergence(paper1,paper2):
     return -np.nansum(paper1 * np.log(paper2/paper1))
 def jensen_shannon_distance(paper1,paper2):
@@ -127,54 +149,76 @@ def jensen_shannon_distance(paper1,paper2):
     return JSD
 
 
-# In[ ]:
-
-
-# jensen_shannon_distance(0.12, 0.74)
-
-
-# In[ ]:
+# In[11]:
 
 
 #initiate individual lists for nodes and links
 node_list = []
 link_list = []
 
+# dist_values = np.full([corpus_df.shape[0], corpus_df.shape[0]], np.nan)
+# dist_values = np.full([1000, 1000], -0.1)
 
-for p1, paper1 in enumerate(corpus_df["Title"]):
+for p1, paper1 in enumerate(corpus_df["Title"][:]):
     grp = {"group" : np.argmax(topic_distributions[p1]), "name": paper1}
     node_list.append(grp)
-    for p2, paper2 in enumerate(corpus_df["Title"]):
+    for p2, paper2 in enumerate(corpus_df["Title"][p1:]):
         if p1 == p2:
             dist = 0
         else:
             #round to 2 decimal places and multiply by 10
-            dist = int(round(1/jensen_shannon_distance(topic_distributions[p1], topic_distributions[p2]), 2)*10)
-        link = {"source": p1, "target": p2, "value": dist}
-        print(link)
-        link_list.append(link)
+            JSD = jensen_shannon_distance(topic_distributions[p1], topic_distributions[p2])
+            dist = round(1/JSD, 2)
+#             dist = round(1/JSD, 2)
+#             if dist >= 0.1 and dist <= 10:
+            link = {"source": p1, "target": p2, "value": dist}
+            link_list.append(link)
+            continue
+
+
+# In[12]:
+
+
+#convert to array and save
+node_array = np.array(node_list)
+link_array = np.array(link_list)
+
+with open('node_array.npy', 'wb') as f:
+    np.save(f, node_array)
+with open('link_array.npy', 'wb') as f:
+    np.save(f, link_array)
 
 
 # In[ ]:
 
 
-#initiate json file
-json_prep = {"links":link_list, "nodes":node_list}
+# # save the lists
+# with open("node_list_full20.txt", "wb") as fp:
+#     pkl.dump(node_list, fp)
+# with open("link_list_full20.txt", "wb") as fp:
+#     pkl.dump(link_list, fp)
 
-#json does not recognize NumPy data types; defining own encoder
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NpEncoder, self).default(obj)
 
-#dumping the data into json file
-json_dump = json.dumps(json_prep, indent=1, sort_keys=True, cls=NpEncoder)
+# In[ ]:
+
+
+# #initiate json file
+# json_prep = {"links":link_list, "nodes":node_list}
+# # json_prep = {"links":link_list}
+# #json does not recognize NumPy data types; defining own encoder
+# class NpEncoder(json.JSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, np.integer):
+#             return int(obj)
+#         elif isinstance(obj, np.floating):
+#             return float(obj)
+#         elif isinstance(obj, np.ndarray):
+#             return obj.tolist()
+#         else:
+#             return super(NpEncoder, self).default(obj)
+
+# #dumping the data into json file
+# json_dump = json.dumps(json_prep, indent=1, sort_keys=True, cls=NpEncoder)
 
 
 # In[ ]:
@@ -192,11 +236,11 @@ json_dump = json.dumps(json_prep, indent=1, sort_keys=True, cls=NpEncoder)
 # In[ ]:
 
 
-#save output
-filename_out = 'hydro_mind.json'
-json_out = open(filename_out,'w')
-json_out.write(json_dump)
-json_out.close()
+# #save output
+# filename_out = 'hiddenstories_full20.json'
+# json_out = open(filename_out,'w')
+# json_out.write(json_dump)
+# json_out.close()
 
 
 # In[ ]:
